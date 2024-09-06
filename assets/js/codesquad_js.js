3f2d97166c8b61bcc19d2fa48436ed21f32d5241
Andrew O <andrew@codesquad.org>
2024-09-06 19:53:42 -0400

    Begin pseudo code for dropdown menu

--- assets/js/codesquad_js.js
+++ assets/js/codesquad_js.js
@@ -21,3 +21,25 @@ function toggleCards(e) {
   let divToChange = e.target.parentElement.parentElement;
   divToChange.classList.toggle('is-flipped');
 };
+
+// w3schools Dropdown Button code:
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
